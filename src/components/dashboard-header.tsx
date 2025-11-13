51b9024a3b531150786b481e39fc95b5e4fc08e2
Rishi Kumarvadada <rishikumarvadada@gmail.com>
2025-11-13 09:26:55 +0000

    dont change anuyting just change the navbar to static no images or anyth

--- src/components/dashboard-header.tsx
+++ src/components/dashboard-header.tsx
@@ -6,7 +6,7 @@ import { UserNav } from '@/components/user-nav';
 
 export default function DashboardHeader() {
   return (
-    <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+    <header className="flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
       <div className="sm:hidden">
         <SidebarTrigger />
       </div>
