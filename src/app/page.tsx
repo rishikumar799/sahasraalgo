b9652b7585643ef8d3e2055a5f76de0a4f56ffcb
Rishi Kumarvadada <rishikumarvadada@gmail.com>
2025-11-17 08:07:08 +0000

    more golden color remingin all ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -161,7 +161,7 @@ export default function Home() {
                       className="z-10 rounded-xl object-cover h-full w-full opacity-90"
                       data-ai-hint={heroPlaceholder.imageHint}
                     />
-                    <div className="absolute inset-0 z-20 rounded-xl bg-gradient-to-tr from-yellow-400/20 to-amber-600/10 mix-blend-overlay"></div>
+                    <div className="absolute inset-0 z-20 rounded-xl bg-gradient-to-tr from-yellow-400/40 to-amber-600/20 mix-blend-overlay"></div>
                   </div>
               )}
             </div>
